41095544d8811a7805646ae68d14cd18d1073b85
sami-amer <59567234+samiatMIT@users.noreply.github.com>
2022-01-03 23:12:39 +0200

    added logging, cleaned up code, base function works

--- data_collector.py
+++ data_collector.py
@@ -7,69 +7,21 @@ from dateutil.relativedelta import relativedelta
 from requests.api import get
 import csv
 
+from helper import get_data
+
+import logging
+
+# logging.basicConfig(filename="helpers.log", encoding = "utf-8", level = logging.DEBUG, filemode='w') # ! Remove filemode W later
+logging.basicConfig(format='%(asctime)s %(message)s')
+
 with open('nyt.api.txt') as f:
     for line in f:
         APIKEY = line
         break
-
-def send_request(url):
-    '''Sends a request to the NYT Archive API for given date.'''
-    response = requests.get(url).json()
-    time.sleep(6)
-    return response
-
-
-def is_valid(subsection, geo_facet):
-    '''An article is only worth checking if it is in range, and has a headline.'''
-    if subsection == "Middle East":
-        # return True if 'palestine' in geo_facet or 'israel' in geo_facet else False
-        return True
-    return False
-
-
-def parse_response(article_json):
-    '''Parses and returns response as pandas data frame.'''
-    data = {
-        'title': article_json['title'] if article_json['title'] else 'EMPTY',
-        'abstract' : article_json['abstract'],
-        'subsection': article_json['subsection'],
-        'author' : article_json['byline'],
-        'publish_date': article_json['published_date'],
-        'updated_date': article_json['updated_date'],
-        'locations': article_json['geo_facet'],
-        'descriptions': article_json['des_facet'],
-        'url':article_json['url']
-    }
-    return data
-
-
-def get_data(url):
-    responses = send_request(url)['results']
-    valid_articles = []
-    for article in responses:
-        if is_valid(article['subsection'],article['geo_facet']):
-            valid_articles.append(parse_response(article))
-    article_data = []
-    for article_json in valid_articles:
-       article_data.append(download_article(article_json['url']))
-    
-    return article_data
-
-def grab_search_term(article_url):
-    return article_url.split('/')[-1].split('-')[0]
-
-def download_article(article_url):
-    search_term = grab_search_term(article_url)
-    fq = 'web_url:("'+str(article_url)+'")'
-    url = f"https://api.nytimes.com/svc/search/v2/articlesearch.json?q='{search_term}'&fq={fq}&api-key={APIKEY}"
-    # print(url)
-    response = send_request(url)['response']["docs"][0]
-    snippet = response["snippet"]
-    lead_paragraph = response["lead_paragraph"]
-    # print(snippet, lead_paragraph)
-    return {"snippet": snippet, "lead_paragraph":lead_paragraph}
+    logging.info("API key loaded in main")
 
 def add_to_csv(data):
+    logging.info("Writing to CSV")
     to_write = [data['snippet'], data['lead_paragraph']] # make the lead pargraph RAW
     with open("data.csv","a") as csvfile:
         writer = csv.writer(csvfile, delimiter=',',
@@ -77,10 +29,13 @@ def add_to_csv(data):
         writer.writerow(to_write)
 
 def main():
+    logging.info("Starting program")
     article_data = get_data(url)
+    # logging.DEBUG(article_data)
     for data in article_data:
-        # print(data)
-        add_to_csv(data)
+        if data:
+            add_to_csv(data)
+
 if __name__ == '__main__':
 
     url = f"https://api.nytimes.com/svc/news/v3/content/nyt/world.json?api-key={APIKEY}"
@@ -88,7 +43,7 @@ if __name__ == '__main__':
     # r = send_request(url)
     # print(r['results'][0])
     # print(get_data(url))
-    
+    # ! FIX THE TEXT
 
     # article_url = "https://www.nytimes.com/2021/12/03/world/middleeast/israel-shira-isakov-domestic-violence.html"
     # download_article(article_url)
